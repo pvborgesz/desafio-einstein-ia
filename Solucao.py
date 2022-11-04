9d439b8cf802c5cb667af214ff2ed88dd148a23f
pvborgesz <pvbo.lima@discente.ufma.br>
2022-11-03 22:10:08 -0300

    Criando classe solução

---
+++ Solucao.py
@@ -0,0 +1,9 @@
+class Solucao:
+    def __init__(self, cor, nacionalidade, bebida, cigarro, animal):
+        self.cor = cor
+        self.nacionalidade = nacionalidade
+        self.bebida = bebida
+        self.cigarro = cigarro
+        self.animal = animal
+    
+       
---
+++ main.py
@@ -0,0 +1,9 @@
+from Solucao import Solucao
+
+class Main:
+    def __init__(self):
+        solucao= Solucao("vermelho", "ingles", "agua", "lucky strike", "cao")
+        # print(solucao)
+
+if __name__ == "__main__":
+    Main()
